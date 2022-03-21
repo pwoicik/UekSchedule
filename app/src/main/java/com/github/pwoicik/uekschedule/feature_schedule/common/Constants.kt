04cef478612cca37edd3dd90686b44bcbbbab055
pwoicik <patrykwoicik@gmail.com>
2022-03-21 20:15:42 +0100

    added couple of constants

--- app/src/main/java/com/github/pwoicik/uekschedule/feature_schedule/common/Constants.kt
+++ app/src/main/java/com/github/pwoicik/uekschedule/feature_schedule/common/Constants.kt
@@ -3,4 +3,6 @@ package com.github.pwoicik.uekschedule.feature_schedule.common
 object Constants {
     const val BASE_URL = "https://planzajec.uek.krakow.pl/"
     const val DATABASE_NAME = "schedule_database"
+    const val CONTACT_EMAIL = "pwoicik.contact@gmail.com"
+    const val GITHUB_REPO_URL = "https://github.com/pwoicik/UEK_Schedule/"
 }
