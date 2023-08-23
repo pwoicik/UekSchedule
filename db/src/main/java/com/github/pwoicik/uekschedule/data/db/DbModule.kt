bee06b2c008352ef35895adfc0cd107b149c1eae
pwoicik <patrykwoicik@gmail.com>
2023-08-23 22:34:05 +0200

    db fix

--- db/src/main/java/com/github/pwoicik/uekschedule/data/db/DbModule.kt
+++ db/src/main/java/com/github/pwoicik/uekschedule/data/db/DbModule.kt
@@ -1,14 +1,10 @@
 package com.github.pwoicik.uekschedule.data.db
 
 import android.app.Application
-import android.content.Context
 import androidx.room.Room
 import com.github.pwoicik.uekschedule.common.Constants
-import com.github.pwoicik.uekschedule.common.domain.PreferencesManager
 import dagger.Module
 import dagger.Provides
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
 import javax.inject.Singleton
 
 @Module
@@ -17,37 +13,13 @@ object DbModule {
     @Provides
     @Singleton
     fun provideDatabase(
-        app: Application,
-        preferences: PreferencesManager
-    ): ScheduleDatabase {
-        clearTablesIfNeeded(app, preferences)
-        return Room
-            .databaseBuilder(
-                app,
-                ScheduleDatabase::class.java,
-                Constants.DATABASE_NAME
-            )
-            .fallbackToDestructiveMigration()
-            .build()
-    }
-
-    /**
-     * Migration2to3 doesn't work when renaming column ignored_subjects.group_id to schedulable_id
-     * as it fills this column with null values for some reason.
-     * To work around it this function clears that table before creating room instance.
-     * TODO: remove after all users migrate from ver. 33
-     */
-    private fun clearTablesIfNeeded(
-        app: Application,
-        preferences: PreferencesManager
-    ) {
-        runBlocking {
-            val lastUsedAppVersion = preferences.lastUsedAppVersion.first() ?: 0
-            if (lastUsedAppVersion < 35) {
-                app.openOrCreateDatabase(Constants.DATABASE_NAME, Context.MODE_PRIVATE, null).use {
-                    it.execSQL("delete from ignored_subjects")
-                }
-            }
-        }
-    }
+        app: Application
+    ) = Room
+        .databaseBuilder(
+            app,
+            ScheduleDatabase::class.java,
+            Constants.DATABASE_NAME
+        )
+        .fallbackToDestructiveMigration()
+        .build()
 }
