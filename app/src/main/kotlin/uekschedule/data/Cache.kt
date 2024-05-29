86434b33ad3f0ec16887e21c689b822133fcc994
pwoicik <patrykwoicik@gmail.com>
2024-05-30 00:05:49 +0200

    fixed cache

--- app/src/main/kotlin/uekschedule/data/Cache.kt
+++ app/src/main/kotlin/uekschedule/data/Cache.kt
@@ -1,12 +1,35 @@
 package uekschedule.data
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.util.reflect.TypeInfo
+import io.ktor.util.reflect.typeInfo
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlin.coroutines.cancellation.CancellationException
 
-val Cache = mutableMapOf<String, Any>()
+private val mutex = Mutex()
+private val Cache = mutableMapOf<String, Any>()
 
-suspend inline fun <reified T : Any> HttpClient.getCacheable(url: String) =
-    Cache[url]?.let { it as T }
-        ?: get(url).body<T>()
-            .also { synchronized(Cache) { Cache[url] = it } }
+suspend inline fun <reified T : Any> HttpClient.getCacheable(url: String): Either<Exception, T> =
+    getCacheable(url, typeInfo<T>())
+
+suspend fun <T : Any> HttpClient.getCacheable(
+    url: String,
+    typeInfo: TypeInfo,
+): Either<Exception, T> {
+    val cached = Cache[url]
+    if (cached != null) {
+        @Suppress("UNCHECKED_CAST")
+        return (cached as T).right()
+    }
+    return try {
+        get(url).body<T>(typeInfo).also { mutex.withLock { Cache[url] = it } }.right()
+    } catch (e: Exception) {
+        if (e is CancellationException) throw e else e.left()
+    }
+}
